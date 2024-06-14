28c732bc7134f13e6ef148e424b131e14980b3fc
shawn <coolrainerseth@gmail.com>
2024-06-14 18:22:25 +0800

    tuning linked queue

--- rs-calculator/src/main.rs
+++ rs-calculator/src/main.rs
@@ -1,6 +1,3 @@
-use std::sync::{Arc, Barrier};
-use std::thread;
-
 mod expression;
 mod lexer;
 mod parser;
@@ -8,24 +5,4 @@ mod token;
 
 fn main() {
     println!("Hello, world!");
-
-    let barrier = Arc::new(Barrier::new(3)); // 创建一个Barrier，需要等待3个线程
-
-    let mut handles = vec![];
-    for i in 0..3 {
-        let c = barrier.clone();
-        let handle = thread::spawn(move || {
-            println!("Thread {:?} before the barrier", thread::current().id());
-            c.wait(); // 等待所有线程都执行到这里
-            println!("Thread {:?} after the barrier", thread::current().id());
-            i
-        });
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        let result = handle.join().unwrap(); // 等待所有线程结束
-        println!("result: {}", result);
-    }
-    println!("All threads have finished");
 }
--- rs-lockless/src/simple/linked_queue_safe.rs
+++ rs-lockless/src/simple/linked_queue_safe.rs
@@ -1,33 +1,121 @@
+use std::borrow::{Borrow, BorrowMut};
 use std::cell::RefCell;
+use std::fmt::{Debug, Display};
+use std::ptr::NonNull;
 use std::rc::Rc;
-
-type NodeRef = Option<Rc<RefCell<Node>>>;
 
 struct Node {
     data: i32,
-    next: NodeRef,
+    next: Option<Box<Node>>,
+}
+
+impl Node {
+    fn new(data: i32) -> Self {
+        Node { data, next: None }
+    }
+}
+
+impl Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.next {
+            Some(node) => {
+                let node = unsafe { node.as_ref() };
+                write!(f, "Node({}) -> {}", self.data, node)
+            }
+            None => write!(f, "Node({}) -> null", self.data),
+        }
+    }
 }
 
 struct LinkedQueue {
-    head: NodeRef,
-    tail: NodeRef,
+    head: Option<Box<Node>>,
+    tail: *mut Node,
     size: usize,
 }
 
 impl LinkedQueue {
     fn new() -> Self {
-        Self { head: None, tail: None, size: 0 }
+        Self { head: None, tail: std::ptr::null_mut(), size: 0 }
     }
 
-    // fn enqueue(&mut self, data: i32) {
-    //     let next_node = self.head.take();
-    //     let new_node = Node { data, next: Box::new(next_node) };
-    //     self.head = Box::new(Some(Node { data: 0, next: Box::new(Some(new_node)) }));
-    //     self.size += 1;
-    // }
+    fn enqueue(&mut self, data: i32) {
+        let mut new_node = Box::new(Node { data, next: None });
+        let raw_ptr: *mut _ = &mut *new_node;
+        if !self.tail.is_null() {
+            unsafe { (*self.tail).next = Some(new_node) };
+        } else {
+            self.head = Some(new_node);
+        }
+        self.tail = raw_ptr;
+        self.size += 1;
+    }
 
-    // fn dequeue(&mut self) -> Option<i32> {
-    //     self.size -= 1;
-    //     None
-    // }
+    fn dequeue(&mut self) -> Option<i32> {
+        self.head.take().map(|head| {
+            let head = *head;
+            self.head = head.next;
+            if self.head.is_none() {
+                self.tail = std::ptr::null_mut();
+            }
+            self.size -= 1;
+            head.data
+        })
+    }
+
+    fn size(&self) -> usize {
+        self.size
+    }
+}
+
+impl Display for LinkedQueue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut current = self.head.as_ref();
+        while let Some(node) = current {
+            write!(f, "{} -> ", node.data);
+            if let Some(next) = &node.next {
+                current = Some(next);
+            } else {
+                current = None;
+            }
+        }
+        write!(f, "None")
+    }
+}
+
+#[test]
+fn test_node() {
+    let mut node = Node { data: 2, next: None };
+    println!("{node}");
+
+    let mut node = Node { data: 3, next: Some(Box::new(node)) };
+    println!("{}", node);
+
+    let node = Node { data: 5, next: Some(Box::new(node)) };
+    println!("{}", node);
+}
+
+#[test]
+fn test_linked_queue() {
+    let mut queue = LinkedQueue::new();
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    assert_eq!(queue.size(), 3);
+    println!("queue is [{}]", queue);
+
+    assert_eq!(queue.dequeue(), Some(1));
+    assert_eq!(queue.size(), 2);
+    println!("queue is [{}]", queue);
+
+    assert_eq!(queue.dequeue(), Some(2));
+    assert_eq!(queue.size(), 1);
+    println!("queue is [{}]", queue);
+
+    assert_eq!(queue.dequeue(), Some(3));
+    assert_eq!(queue.size(), 0);
+    println!("queue is [{}]", queue);
+
+    assert_eq!(queue.dequeue(), None);
+    assert_eq!(queue.size(), 0);
+    println!("queue is [{}]", queue);
 }
